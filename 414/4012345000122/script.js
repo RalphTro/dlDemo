eeec38bd80f28eb41999a5f223b6c8eac31a1b10
RalphTro <46958354+RalphTro@users.noreply.github.com>
2024-08-14 11:01:45 +0200

    Included polygon

--- 414/4012345000122/script.js
+++ 414/4012345000122/script.js
@@ -5,7 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([50.942499, 6.898247]).addTo(map)
+    const marker = L.marker([50.942499, 6.898247]).addTo(map)
         .bindPopup('Example Location One')
         .openPopup();
+
+    const polygon = L.polygon([
+        [50.942499, 6.898247],
+        [50.942275, 6.898292],
+        [50.942263, 6.898094],
+        [50.942106, 6.898126],
+        [50.942130, 6.898526],
+        [50.942512, 6.898451],
+        [50.942499, 6.898247]
+    ]).addTo(map);
 });
